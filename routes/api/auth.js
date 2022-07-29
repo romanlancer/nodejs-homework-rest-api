e4ed677fbd7b638202b600eb4061f60323cf9f34
Roman Hyrych <roman.girich@gmail.com>
2022-07-29 18:16:06 +0300

    created user auth

---
+++ routes/api/auth.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const { basedir } = global
+
+const ctrl = require(`${basedir}/controllers/auth`)
+
+const { controllersWrapper } = require(`${basedir}/helpers`)
+
+const { auth } = require(`${basedir}/middlewares`)
+
+const router = express.Router()
+
+router.post('/register', controllersWrapper(ctrl.register))
+
+router.post('/login', controllersWrapper(ctrl.login))
+
+router.get('/current', auth, controllersWrapper(ctrl.getCurrent))
+
+router.get('/logout', auth, controllersWrapper(ctrl.logout))
+
+module.exports = router
